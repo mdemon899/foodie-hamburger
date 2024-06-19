5ae7f78c15a8cca5085c415515fb8f152bacd389
MD. NORE ALOM EMON <82075395+mdemon899@users.noreply.github.com>
2024-06-19 22:30:35 +0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const closeIcon = document.querySelector(".close-icon");
 const navBar = document.querySelector("#navbar ul");
+const goToTop = document.querySelector(".go-to-top a")
 
 hamburgerMenu.addEventListener("click", (e) => {
   navBar.classList.add("menu-open");
@@ -17,4 +18,9 @@ closeIcon.addEventListener("click", () => {
 document.addEventListener("click", () => {
   navBar.classList.remove("menu-open");
   hamburgerMenu.classList.remove("hidden");
+});
+
+goToTop.addEventListener("click", (e) => {
+  document.querySelector("#main-container").scrollTo(0,0)
+  e.stopPropagation();
 });
